da44e029633f6385a023b05af82d5f0d0645909a
k40l1n <droidaarqam@gmail.com>
2021-01-30 05:28:27 +0530

    Markdown App added

--- router.js
+++ router.js
@@ -3,12 +3,14 @@ import {createRouter, createWebHistory} from 'vue-router';
 import Home from './src/pages/Home';
 import Heroes from './src/pages/Heroes';
 import Calendar from './src/pages/Calendar';
+import Markdown from './src/pages/Markdown';
 
 
 const routes = [
   {path: '/', component:Home},
   {path: '/heroes', component:Heroes},
   {path: '/calendar', component:Calendar},
+  {path: '/markdown', component:Markdown},
 ]
 
 const router = createRouter({
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,12 +1,21 @@
 <template>
   <nav class="w-full text-white bg-blue-800 px-4 py-2">
-    <router-link class="mx-2" to="/heroes">DC Heroes</router-link>
-    <router-link class="mx-2" to="/calendar">Calendar</router-link>
+    <router-link v-for="item in list" :key="item.to" class="mx-2" :to="item.to">{{ item.title }}</router-link>
   </nav>
 </template>
 
 <script>
-export default {};
+export default {
+  data(){
+    return {
+      list: [
+        {title: 'Heroes', to: '/heroes'},
+        {title: 'Calendar', to: '/calendar'},
+        {title: 'Markdown', to: '/markdown'}
+      ]
+    }
+  }
+};
 </script>
 
 <style></style>
